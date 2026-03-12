2690054ac12b91374790652e78e687bfdf4a0ee6
adedigba goodness <adedigbagoodness@gmail.com>
2026-03-12 03:42:53 -0700

    push

--- index.js
+++ index.js
@@ -1,68 +1,71 @@
 const express = require('express');
 const app = express();
 const port = 2001;
+
 const students=[
     {
         id:1,
-        Tile:'its going'
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
     },
-      {
-        id:1,
-        Tile:'its going'
-        Name:'John Doe',
-        Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
-    },
-      {
+    {
         id:2,
-        Tile:'its going'
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
     },
-      {
+    {
         id:3,
-        Tile:'its going'
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
     },
-      {
+    {
         id:4,
-        Tile:'its going'
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
     },
-      {
+    {
         id:5,
-        Tile:'its going'
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
     },
-      {
-        id:6,
-        Tile:'its going'
+    {
+        id:5,
+        Title:'its going',
         Name:'John Doe',
         Duration:20,
-        grade:'2mins 41secs'
-        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
-        picture link:''
+    },
+    {
+        id:6,
+        Title:'its going',
+        Name:'John Doe',
+        Duration:20,
     },
 ]
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+    
+})
+app.get('/students',(req,res)=>{
+    res.json(students);
+    console.log('welcome');
+    
+});
+
+
+// app.listen(port,()=>{
+//     console.log("welcome");
+    
+// })
+
+
+// app.get("/students",(req,res)=>{
+//     res.sendFile(__dirname + "/index.html")
+//     // C:\Users\HomePC\Desktop\web development\level 3\class one\index.html
+// });
