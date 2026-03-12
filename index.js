7da088f8d5f13a1086339b26fa6c582f52bf8c8b
adedigba goodness <adedigbagoodness@gmail.com>
2026-03-12 00:35:46 -0700

    class one

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const port = 2001;
+const students=[
+    {
+        id:1,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:1,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:2,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:3,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:4,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:5,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+      {
+        id:6,
+        Tile:'its going'
+        Name:'John Doe',
+        Duration:20,
+        grade:'2mins 41secs'
+        song link:'https://www.youtube.com/watch?v=QH2-TGUlwu4'
+        picture link:''
+    },
+]
